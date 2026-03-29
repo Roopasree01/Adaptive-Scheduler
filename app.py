9331031fec4fe55d9a17b71235ce0abe4ecedf45
Roopasree Challa <roopasree01.challa@gmail.com>
2026-03-29 17:00:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from core.process import Process
 from core.system import AdaptiveSystem
+import os
 
 app = Flask(__name__)
 
@@ -49,4 +50,5 @@ def run_simulation():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
